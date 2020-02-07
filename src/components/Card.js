137b926f2cfcb88f405cbf4f6cd75ff418c5564e
lordrichard <pauloreizinho@hotmail.com>
2020-02-07 14:11:07 +0100

    robo size upgraded

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='card tc grow br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?size=300x300`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -28,7 +28,7 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
-      <h1>Loading</h1> :
+      <h1 className='tc'>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f-headline ma4'>RoboFriends</h1>
